ef41fb645e1df6c6dc72a70f298ac137a828b7a2
Miroslav <miroslavmironov@gmail.com>
2017-03-14 10:46:35 +0200

    Added a FloatingActionListener so that action events can be triggered for the KolibriFAB.

---
+++ sdk/src/main/java/ch/yanova/kolibri/FloatingActionListener.java
@@ -0,0 +1,9 @@
+package ch.yanova.kolibri;
+
+/**
+ * Created by mmironov on 3/14/17.
+ */
+
+public interface FloatingActionListener {
+    void onVisibilityChanged(boolean visible);
+}
--- sdk/src/main/java/ch/yanova/kolibri/components/KolibriFloatingActionButton.java
+++ sdk/src/main/java/ch/yanova/kolibri/components/KolibriFloatingActionButton.java
@@ -3,8 +3,11 @@ package ch.yanova.kolibri.components;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.preference.Preference;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
+
+import ch.yanova.kolibri.FloatingActionListener;
 
 /**
  * Created by mmironov on 3/10/17.
@@ -16,6 +19,12 @@ public class KolibriFloatingActionButton extends FloatingActionButton implements
     public static final String URI_HIDE = "kolibri://fab/hide";
 
     private String urlToBookmark;
+
+    public void setFloatingActionListener(FloatingActionListener listener) {
+        this.listener = listener;
+    }
+
+    private FloatingActionListener listener;
 
     public KolibriFloatingActionButton(Context context) {
         super(context);
@@ -50,10 +59,22 @@ public class KolibriFloatingActionButton extends FloatingActionButton implements
 
         if (intent.getDataString().startsWith(URI_SHOW)) {
             show();
+
+            if (listener != null) {
+                listener.onVisibilityChanged(true);
+            }
         }
 
         if (intent.getDataString().startsWith(URI_HIDE)) {
             hide();
+
+            if (listener != null) {
+                listener.onVisibilityChanged(false);
+            }
         }
+    }
+
+    public void onVisibilityChanged(boolean visible) {
+
     }
 }
