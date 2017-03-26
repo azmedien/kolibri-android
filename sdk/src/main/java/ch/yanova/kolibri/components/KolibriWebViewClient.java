154a968cacdaa4456dbf5d9b196e22c3f5da6ffd
Miroslav <miroslavmironov@gmail.com>
2017-03-26 21:38:32 +0300

    Chaged the asserts with null checks in ifs.

--- sdk/src/main/java/ch/yanova/kolibri/components/KolibriWebViewClient.java
+++ sdk/src/main/java/ch/yanova/kolibri/components/KolibriWebViewClient.java
@@ -125,23 +125,26 @@ public class KolibriWebViewClient extends WebViewClient {
     @Override
     public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
         super.onReceivedError(view, request, error);
-        assert listener != null;
-        listener.onReceivedError(view, request, error);
+        if (listener != null) {
+            listener.onReceivedError(view, request, error);
+        }
 
     }
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
         super.onPageStarted(view, url, favicon);
-        assert listener != null;
-        listener.onPageStarted(view, url, favicon);
+        if (listener != null) {
+            listener.onPageStarted(view, url, favicon);
+        }
     }
 
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
-        assert listener != null;
-        listener.onPageFinished(view, url);
+        if (listener != null) {
+            listener.onPageFinished(view, url);
+        }
     }
 
     boolean handleUri(KolibriWebView view, Context context, Uri link) {
