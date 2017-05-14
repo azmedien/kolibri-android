13241931959cf879dd329182a169ba14d1d60424
Miroslav <miroslavmironov@gmail.com>
2017-05-14 16:18:09 +0300

    Added Search to the Prototype and also a check for no search query parameter in the SearchWebViewCoordinator.

--- prototype/src/main/java/ch/yanova/kolibri/prototype/MainActivity.java
+++ prototype/src/main/java/ch/yanova/kolibri/prototype/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
@@ -13,11 +17,14 @@ import ch.yanova.kolibri.KolibriCoordinator;
 import ch.yanova.kolibri.KolibriNavigationActivity;
 import ch.yanova.kolibri.KolibriProvider;
 import ch.yanova.kolibri.components.KolibriWebView;
+import ch.yanova.kolibri.coordinators.SearchWebviewCoordinator;
 import ch.yanova.kolibri.coordinators.WebViewCoordinator;
 import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends KolibriNavigationActivity implements View.OnClickListener {
+
+    private KolibriWebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +41,51 @@ public class MainActivity extends KolibriNavigationActivity implements View.OnCl
 
     @Override
     public Fragment onPostInitialize() {
-        return WebViewFragment.newInstance();
+        return WebViewFragment.newInstance("");
     }
 
     @Override
     public void onBindComponents() {
         final WebViewFragment webViewFragment = (WebViewFragment) getMainFragment();
-        final KolibriWebView webView = webViewFragment.getWebview();
+        webView = webViewFragment.getWebView();
 
         Kolibri.bind(webView, new KolibriProvider() {
             @Nullable
             @Override
             public KolibriCoordinator provideCoordinator(View view) {
-                return new WebViewCoordinator(null);
+                return new SearchWebviewCoordinator(null, webViewFragment);
             }
         });
     }
 
     @Override
     public void onNavigationInitialize() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getWebViewCoordinator().onQueryByText(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private SearchWebviewCoordinator getWebViewCoordinator() {
+        return ((SearchWebviewCoordinator)webView.getTag(R.id.coordinator));
     }
 }
--- prototype/src/main/java/ch/yanova/kolibri/prototype/WebViewFragment.java
+++ prototype/src/main/java/ch/yanova/kolibri/prototype/WebViewFragment.java
@@ -1,47 +1,105 @@
 package ch.yanova.kolibri.prototype;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 
-import ch.yanova.kolibri.KolibriFragment;
+import java.util.Map;
+
+import ch.yanova.kolibri.KolibriLoadingFragment;
 import ch.yanova.kolibri.components.KolibriWebView;
+import ch.yanova.kolibri.components.KolibriWebViewClient;
+import ch.yanova.kolibri.components.OnAmpDataFoundListener;
+import ch.yanova.kolibri.coordinators.WebViewCoordinator;
 
 /**
- * Created by lekov on 4/10/17.
+ * Created by lekov on 4/2/17.
  */
 
-public class WebViewFragment extends KolibriFragment {
+public class WebViewFragment extends KolibriLoadingFragment implements KolibriWebViewClient.WebClientListener, OnAmpDataFoundListener {
 
+    private Intent shareIntent;
 
-    private KolibriWebView webview;
+    KolibriWebView getWebView() {
+        return webView;
+    }
 
-    public static WebViewFragment newInstance() {
+    private KolibriWebView webView;
+
+    public static WebViewFragment newInstance(String url) {
 
         Bundle args = new Bundle();
+        args.putString("url", url);
 
         WebViewFragment fragment = new WebViewFragment();
         fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.web_fragment, container, false);
+        return fragment;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        webview = (KolibriWebView) view.findViewById(R.id.webview);
+        webView = (KolibriWebView) view.findViewById(R.id.webview);
+        webView.setTag(KolibriWebView.class);
+        webView.setWebClientListener(this);
+
+        int color = ContextCompat.getColor(getContext(), R.color.colorPrimary);
+        setProgressColor(color);
+
+        if (getArguments().containsKey("url")) {
+            final String url = getArguments().getString("url");
+            webView.loadUrl(url);
+        }
     }
 
-    public KolibriWebView getWebview() {
-        return webview;
+    @NonNull
+    @Override
+    protected View getMainContentView(LayoutInflater inflater, ViewGroup container) {
+        return inflater.inflate(R.layout.web_fragment, container, false);
     }
 
+    @Override
+    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        final String errorMessage = error != null ? error.toString() : null;
 
+        showPageError(errorMessage, "Click", null);
+    }
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        showPageLoading();
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        showPage();
+    }
+
+    @Override
+    public boolean shouldHandleInternal() {
+        return true;
+    }
+
+    @Override
+    public void onFound(Map<String, String> data) {
+        if (data.size() > 0 && data.containsKey(WebViewCoordinator.FAV_LABEL)) {
+            final String url = data.get(WebViewCoordinator.META_CANONICAL_URL);
+
+            shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, data.get(WebViewCoordinator.FAV_LABEL));
+            shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+        }
+    }
 }
