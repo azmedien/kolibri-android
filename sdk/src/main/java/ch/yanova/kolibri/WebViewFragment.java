e314215b656858f5260d2ca4159af4b075f7aa8d
Miroslav <miroslavmironov@gmail.com>
2017-09-29 16:55:15 +0300

    Adds invalidating of the options menu every time a new page has been started.

--- sdk/src/main/java/ch/yanova/kolibri/WebViewFragment.java
+++ sdk/src/main/java/ch/yanova/kolibri/WebViewFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -98,6 +99,7 @@ public class WebViewFragment extends KolibriLoadingFragment implements WebViewLi
             isThemeTinted = false;
         }
 
+        ActivityCompat.invalidateOptionsMenu(getActivity());
         showPageLoading();
     }
 
