f7e87bfed94ac326356504e6d26bfe4e71f59762
Asen Lekov <asenlekoff@gmail.com>
2018-05-25 23:47:27 +0300

    Testing
    Fixed some broken tests
    Remove wrong test
    Extract constants

---
+++ sdk/src/test/java/ch/yanova/kolibri/Constants.java
@@ -0,0 +1,6 @@
+package ch.yanova.kolibri;
+
+public class Constants {
+  public static final String TEST_CONFIG_RES = "test.json";
+  public static final String APP_URL = "https://kolibri.herokuapp.com/apps/7yaaRbHQx2NvYTori9EWqazJ";
+}
